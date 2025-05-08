975677fa2ce0ff44092d3afb7bf9e48fa747c766
Ebe <ebenezerdsouza27@gmail.com>
2025-05-08 21:45:08 +0530

    fixed foldername

---
+++ WEBDEV/Week 14/Practice/dist/index.js
@@ -0,0 +1,41 @@
+"use strict";
+// function TS(name:String){
+//     console.log('Hello',name);
+// }
+// TS('Ebe');
+// function Sum(one:number,two:number){
+//     const sum:number=one+two;
+//     return sum;
+// }
+// const ans=Sum(5, 5);
+// console.log(ans);
+// function IsLegal(Age:number): Boolean{
+//     if(Age>=18){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// const result=IsLegal(13);
+// console.log(result?'Eligible':'Not Eligible');
+// function DelayedCall(fn:()=>void){
+//     setTimeout(fn,5000)
+// }
+// DelayedCall(()=>console.log('hi there'));
+function Call(user) {
+    console.log(user.name);
+}
+let user = {
+    name: 'ebe',
+    age: 20
+};
+function Call2(user2) {
+    console.log(user2.name);
+}
+const user0 = {
+    name: 'ebe',
+    age: 20
+};
+Call(user);
+Call2(user0);
